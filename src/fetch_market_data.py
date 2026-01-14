3dd5b27f08f2acbbf71bc22923962118ae77f734
cjselfridge <106233626+cjselfridge@users.noreply.github.com>
2026-01-14 06:55:37 -0800

    Update fetch_market_data.py

--- src/fetch_market_data.py
+++ src/fetch_market_data.py
@@ -1,6 +1,12 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime, timedelta
 
 tickers = {
   "WTI Crude": "CL=F"
 }
+
+def check_data():
+  print("Starting Data Check...")
+
+check_data()
